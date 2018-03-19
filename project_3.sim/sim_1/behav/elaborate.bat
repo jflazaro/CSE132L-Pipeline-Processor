8f4f46a9ad7fa201c334ae6ff7b3414884d1f978
Joshua Lazaro <jflazaro@uci.edu>
2018-03-19 00:24:39 -0700

    Removed sim folder

--- project_3.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xelab  -wto f15e5ab01a5343279c9586949cdd4bdd -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot tb_top_behav xil_defaultlib.tb_top xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0

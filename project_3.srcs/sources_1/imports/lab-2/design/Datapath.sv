a5dad1b101b8e3d9e8a0ee5396fe7b096141a5ef
Joshua Lazaro <jflazaro@uci.edu>
2018-03-19 00:22:19 -0700

    EXMEM Register WIP

--- project_3.srcs/sources_1/imports/lab-2/design/Datapath.sv
+++ project_3.srcs/sources_1/imports/lab-2/design/Datapath.sv
@@ -66,14 +66,22 @@ logic [2:0] Funct3_IDEX_Out;
 logic [RF_ADDRESS-1:0] RDest_IDEX_Out, RS1_IDEX_Out, RS2_IDEX_Out;
 logic [ALU_CC_W-1:0] ALU_CC_IDEX_Out;
 
+//// EX/MEM Wires
+logic MemtoReg_EXM_Out, RegWrite_EXM_Out;
+logic [2:0] MemRead_EXM_Out;
+logic [1:0] MemWrite_EXM_Out;
+logic [DATA_W-1:0] Zero_EXM_Out, ALUResult_EXM_Out, ALUResult_inter_EXM_Out, Reg2_EXM_Out;
+logic [RF_ADDRESS-1:0] RDest_EXM_Out;
+logic [PC_W-1:0] PC_EXM_Out, PCPlus4_EXM_Out;
+
 //// IF/ID Reg
 IfIdReg ifidreg(clk, PC_IFID_In, PCPlus4_IFID_In, Instr_IFID_In, PC_IFID_Out, PCPlus4_IFID_Out, Instr_IFID_Out);
 
 //// next PC
 adder #(9) pcadd (PC_IFID_In, 9'b100, PCPlus4_IFID_In);
 flopr #(9) pcreg(clk, reset, PC_actual, PC_IFID_In);
-mux2  #(9) pcmux(PCPlus4_IFID_In, (PC_IFID_In + (($signed({ExtImm[DATA_W-1],ExtImm[7:0]})))), ((Branch & Zero)/*BR*/ || opcode == 7'b1101111/*JAL*/), PC_inter);
-mux2  #(9) pcmux2(PC_inter, ({ALUResult[DATA_W-1],ALUResult[7:0]} & ~(9'b1)), (opcode == 7'b1100111/*JALR*/), PC_actual);
+mux2  #(9) pcmux(PCPlus4_IFID_In, (PC_EXM_Out + (($signed({ExtImm_IDEX_Out[DATA_W-1],ExtImm_IDEX_Out[7:0]})))), ((Branch & Zero_EXM_Out)/*BR*/ || opcode == 7'b1101111/*JAL*/), PC_inter);
+mux2  #(9) pcmux2(PC_inter, ({ALUResult_EXM_Out[DATA_W-1],ALUResult_EXM_Out[7:0]} & ~(9'b1)), (opcode == 7'b1100111/*JALR*/), PC_actual);
 
 //Instruction memory
 instructionmemory instr_mem (PC_IFID_In, Instr_IFID_In);
@@ -107,12 +115,30 @@ IdExReg idexreg(
     ALU_CC_IDEX_Out
 );
 
+//// EX/MEM Reg
+ExMemReg exmemreg(
+    clk,
+    MemtoReg_IDEX_Out, RegWrite_IDEX_Out,
+    MemRead_IDEX_Out,
+    MemWrite_IDEX_Out,
+    Zero, ALUResult, ALUResult_inter, Reg2_IDEX_Out, 
+    RDest_IDEX_Out,
+    PC_IDEX_Out, PCPlus4_IDEX_Out,
+    
+    MemtoReg_EXM_Out, RegWrite_EXM_Out,
+    MemRead_EXM_Out,
+    MemWrite_EXM_Out,
+    Zero_EXM_Out, ALUResult_EXM_Out, ALUResult_inter_EXM_Out, Reg2_EXM_Out, 
+    RDest_EXM_Out,
+    PC_EXM_Out, PCPlus4_EXM_Out
+);
+
       
 // //Register File
-RegFile rf(clk, reset, RegWrite_IDEX_Out, RDest_IDEX_Out, RS1, RS2,
+RegFile rf(clk, reset, RegWrite_EXM_Out, RDest_EXM_Out, RS1, RS2,
         Result, Reg1, Reg2);
         
-mux2 #(32) resmux(ALUResult_inter, ReadData, MemtoReg_IDEX_Out, Result_inter);
+mux2 #(32) resmux(ALUResult_inter_EXM_Out, ReadData, MemtoReg_EXM_Out, Result_inter);
 mux2 #(32) resmux2(Result_inter, (ExtImm_IDEX_Out + PC_IDEX_Out), (opcode == 7'b0010111/*AUIPC*/), Result);
            
 //// sign extend
@@ -126,6 +152,6 @@ alu alu_module(Reg1_IDEX_Out, SrcB, ALU_CC_IDEX_Out, ALUResult, Zero);
 assign WB_Data = Result;
 
 ////// Data memory 
-datamemory data_mem (clk, MemRead_IDEX_Out, MemWrite_IDEX_Out, ALUResult_inter[DM_ADDRESS-1:0], Reg2_IDEX_Out, ReadData);
+datamemory data_mem (clk, MemRead_EXM_Out, MemWrite_EXM_Out, ALUResult_inter_EXM_Out[DM_ADDRESS-1:0], Reg2_EXM_Out, ReadData);
      
 endmodule
---
+++ project_3.srcs/sources_1/new/ExMemReg.sv
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/18/2018 11:13:49 PM
+// Design Name: 
+// Module Name: ExMemReg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ExMemReg #(
+    parameter PC_W = 9, // Program Counter
+    parameter RF_ADDRESS = 5, // Register File Address
+    parameter DATA_W = 32 // Data WriteData
+    )(
+    input clk,
+    
+    input logic MemtoReg, RegWrite,
+    input logic [2:0] MemRead,
+    input logic [1:0] MemWrite,
+    input logic [DATA_W-1:0] Zero, ALUResult, ALUResult_Inter, Reg2, 
+    input logic [RF_ADDRESS-1:0] RDest,
+    input logic [PC_W-1:0] PC, PCPlus4,
+    
+    output logic MemtoReg_EXM_Out, RegWrite_EXM_Out,
+    output logic [2:0] MemRead_EXM_Out,
+    output logic [1:0] MemWrite_EXM_Out,
+    output logic [DATA_W-1:0] Zero_EXM_Out, ALUResult_EXM_Out, ALUResult_Inter_EXM_Out, Reg2_EXM_Out, 
+    output logic [RF_ADDRESS-1:0] RDest_EXM_Out,
+    output logic [PC_W-1:0] PC_EXM_Out, PCPlus4_EXM_Out
+    );
+
+assign MemtoReg_EXM_Out = MemtoReg;
+assign RegWrite_EXM_Out = RegWrite;
+assign MemRead_EXM_Out = MemRead;
+assign MemWrite_EXM_Out = MemWrite;
+assign Zero_EXM_Out = Zero;
+assign ALUResult_EXM_Out = ALUResult;
+assign ALUResult_Inter_EXM_Out = ALUResult_Inter;
+assign Reg2_EXM_Out = Reg2;
+assign RDest_EXM_Out = RDest;
+assign PC_EXM_Out = PC;
+assign PCPlus4_EXM_Out = PCPlus4;
+
+endmodule
